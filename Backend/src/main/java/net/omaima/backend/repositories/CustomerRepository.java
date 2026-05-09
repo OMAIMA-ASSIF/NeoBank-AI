e8d8c0ddd12148c8bb2cee94954ece5c11d54fde
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-05-09 01:25:45 +0100

    adding search method by keyword 4 a customer

--- Backend/src/main/java/net/omaima/backend/repositories/CustomerRepository.java
+++ Backend/src/main/java/net/omaima/backend/repositories/CustomerRepository.java
@@ -2,6 +2,12 @@ package net.omaima.backend.repositories;
 
 import net.omaima.backend.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    @Query("select c from customer c where c.name %like : kw ")
+    List<Customer> searchCustomers(@Param("kw") String keyword);
 }
