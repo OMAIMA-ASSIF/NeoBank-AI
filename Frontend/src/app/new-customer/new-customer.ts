229f2b6440a504c261e717fe927a511bf59e2a8d
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-05-09 17:17:20 +0100

    adding new customer

--- Frontend/src/app/new-customer/new-customer.ts
+++ Frontend/src/app/new-customer/new-customer.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomerService } from '../services/customer';
 
 @Component({
   selector: 'app-new-customer',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-customer.html',
   styleUrl: './new-customer.css',
 })
-export class NewCustomer {
+export class NewCustomer implements OnInit{
+  newCustomerFormGroup! : FormGroup;
+  constructor(private fb : FormBuilder, private customerService : CustomerService){}
+
+  ngOnInit() : void {
+    this.newCustomerFormGroup = this.fb.group({
+      name : this.fb.control(null, [Validators.required, Validators.minLength(4)]),
+      email : this.fb.control(null, [Validators.required, Validators.email]),
+    });
+  }
+
+  handleSaveCustomer() {
+    let customer = this.newCustomerFormGroup.value;
+    this.customerService.saveCustomer(customer).subscribe({
+      next : (data) => {
+        alert("Customer has been saved successfully !");
+      },
+      error : (err) => {
+        console.log(err);
+         alert("An error has occured while saving customer !");
+      }
+    })
+  }
 
 }
