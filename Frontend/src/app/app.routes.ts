8258b941788db1b1a33bd9de01558359d57b170b
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-05-24 13:29:55 +0100

    add guards

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -5,15 +5,17 @@ import { NewCustomer } from './new-customer/new-customer';
 import { CustomerAccounts } from './customer-accounts/customer-accounts';
 import { Login } from './login/login';
 import { AdminTemplate } from './admin-template/admin-template';
+import { AuthenticationGuard } from './guards/authentification-guard';
 
 export const routes: Routes = [
   {path : "login", component : Login},
   {path : "", redirectTo : "login", pathMatch : "full"},
-  {path : "admin", component : AdminTemplate, children : [
-    {path : "customers", component : Customers},
-    {path : "accounts", component : Accounts},
-    {path : "new-customer", component : NewCustomer},
-    {path : "customer-accounts/:id", component : CustomerAccounts}
-  ]},
+  {path : "admin", component : AdminTemplate, canActivate : [AuthenticationGuard],
+    children : [
+      {path : "customers", component : Customers},
+      {path : "accounts", component : Accounts},
+      {path : "new-customer", component : NewCustomer},
+      {path : "customer-accounts/:id", component : CustomerAccounts}
+    ]},
 
 ];
---
+++ Frontend/src/app/guards/authentification-guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth-service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class AuthenticationGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router ) {
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean {
+    if(this.authService.isAuthenticated==true){
+      return true;
+    } else {
+      this.router.navigateByUrl("/login");
+      return false;
+    }
+  }
+}
